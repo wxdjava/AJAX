34137861a4cde5ed05890b155a558920bbad9d96
wangxuedeng <1518785405@qq.com>
2023-01-08 10:27:55 +0800

    second commit

---
+++ jQuery/src/com/bjpowernode/bean/Test.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.bean;
+
+/**
+ * @author wangxuedeng
+ * @date 2023/1/8 - 10:26
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+}
