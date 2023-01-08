57e60981529d4ca500bb006fab2ba5d875e782e2
wangxuedeng <1518785405@qq.com>
2023-01-08 10:28:52 +0800

    third commit

--- jQuery/src/com/bjpowernode/bean/Test.java
+++ jQuery/src/com/bjpowernode/bean/Test.java
@@ -7,5 +7,6 @@ package com.bjpowernode.bean;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
